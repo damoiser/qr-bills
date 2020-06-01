64c13f0296d4205dd0255d36c5e0e34dc8b8d887
Damiano Radice <dam.radice@gmail.com>
2020-06-01 17:13:29 +0200

    reorder params init

--- lib/qr-bills.rb
+++ lib/qr-bills.rb
@@ -1,29 +1,4 @@
 class QRBills
-  def get_qr_params
-    { 
-      language: "it",
-      qr_content: "",
-      iban: "",
-      address: {
-        street: "",
-        zip: "",
-        city: "",
-      },
-      reference: "",
-      currency: "CHF",
-      amount: 0.0,
-      additionally_information: "",
-      customer: {
-        name: "",
-        address: {
-          street: "",
-          zip: "",
-          city: "",
-        },
-      },
-    }
-  end
-
   def self.create_orange_deposit_slip(qr_params)
 
   end
