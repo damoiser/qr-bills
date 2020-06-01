a9b191cdfc6c3d4aad54c3222ec2ec9b61d430ac
Damiano Radice <dam.radice@gmail.com>
2020-06-01 14:37:57 +0200

    start with init params

--- lib/qr-bills.rb
+++ lib/qr-bills.rb
@@ -1,5 +1,36 @@
 class QRBills
-  def self.hi
-    puts "Hello world!"
+  
+  def get_qr_params
+    { 
+      language: "it",
+      qr_content: "",
+      iban: "",
+      address: {
+        street: "",
+        zip: "",
+        city: "",
+      },
+      reference: "",
+      currency: "CHF",
+      amount: 0.0,
+      additionally_information: "",
+      customer: {
+        name: "",
+        address: {
+          street: "",
+          zip: "",
+          city: "",
+        },
+      },
+    }
   end
+
+  def self.create_orange_deposit_slip(qr_params)
+
+  end
+
+  def self.create_red_deposit_slip(qr_params)
+
+  end
+
 end
