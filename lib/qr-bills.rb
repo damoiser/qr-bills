35f6d56960a5a042eb97561a80e858954409d011
Damiano Radice <dam.radice@gmail.com>
2020-07-01 00:19:35 +0200

    add qr bills types

--- lib/qr-bills.rb
+++ lib/qr-bills.rb
@@ -1,4 +1,14 @@
 class QRBills
+
+  # type of bills
+  QR_BILL_WITH_QR_REFERENCE       = "orange_with_reference"
+  QR_BILL_WITH_CREDITOR_REFERENCE = "red_with_reference"
+  QR_BILL_WITOUTH_REFERENCE       = "red_without_reference"
+
+  def initialize(qr_params)
+
+  end
+
   def self.create_orange_deposit_slip(qr_params)
 
   end
