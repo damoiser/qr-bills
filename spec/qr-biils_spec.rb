14ed38554e3d792ef3927e65fd632b7bfd68ab46
Damiano Radice <dam.radice@gmail.com>
2020-07-01 01:13:28 +0200

    begin with tests

---
+++ spec/qr-biils_spec.rb
@@ -0,0 +1,13 @@
+require 'qr-bills'
+
+RSpec.describe QRBills do
+  describe "init" do
+    it "raise an exception if the bill kind is not set" do
+
+    end
+  
+    it "raise an exception if the bill kind is not valid" do
+
+    end
+  end
+end
