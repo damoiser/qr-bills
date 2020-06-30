4b4750b87ae6a9507dec4a82300b0eba6a6a8890
Damiano Radice <dam.radice@gmail.com>
2020-07-01 01:01:31 +0200

    init generators

--- lib/qr-bills.rb
+++ lib/qr-bills.rb
@@ -1,5 +1,4 @@
 class QRBills
-
   # type of bills
   QR_BILL_WITH_QR_REFERENCE       = "orange_with_reference"
   QR_BILL_WITH_CREDITOR_REFERENCE = "red_with_reference"
@@ -10,13 +9,15 @@ class QRBills
     I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
     I18n.default_locale = :it
 
+    bill = nil
+
     if qr_params.has_key?(:bill_type)
       if qr_params[:bill_type] == QR_BILL_WITH_QR_REFERENCE
-
+        # todo
       elsif qr_params[:bill_type] == QR_BILL_WITH_CREDITOR_REFERENCE
-
+        bill = QRGeneratorWithCreditorReference.new(params)
       elsif qr_params[:bill_type] == QR_BILL_WITOUTH_REFERENCE
-
+        bill = QRGeneratorWithoutReference.new(params)
       else
         raise QRExceptions::INVALID_PARAMETERS + ": bill type not valid"
       end
@@ -24,4 +25,6 @@ class QRBills
       raise QRExceptions::INVALID_PARAMETERS + ": bill type param not set"
     end
   end
+
+  return bill
 end
