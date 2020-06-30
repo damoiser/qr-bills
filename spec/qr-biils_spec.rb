c26703e82ea249b318ebc7e880d464391a136240
Damiano Radice <dam.radice@gmail.com>
2020-07-01 01:25:28 +0200

    add init tests

--- spec/qr-biils_spec.rb
+++ spec/qr-biils_spec.rb
@@ -3,11 +3,11 @@ require 'qr-bills'
 RSpec.describe QRBills do
   describe "init" do
     it "raise an exception if the bill kind is not set" do
-
+      expect{QRBills.new({})}.to raise_error("QR-bill invalid parameters: bill type param not set")
     end
   
     it "raise an exception if the bill kind is not valid" do
-
+      expect{QRBills.new({bill_type: "bad"})}.to raise_error("QR-bill invalid parameters: bill type not valid")
     end
   end
 end
