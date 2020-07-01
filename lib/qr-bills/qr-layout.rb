e73ee2d4ffe19323d0a40c9358a8726a687837a8
Damiano Radice <dam.radice@gmail.com>
2020-07-01 17:52:29 +0200

    draft qr-layout generator

---
+++ lib/qr-bills/qr-layout.rb
@@ -0,0 +1,22 @@
+class QRLayout
+
+  def html_layout
+    layout = `
+      <div class="bill_container">
+        <div class="receipt">
+
+        </div>
+        <div class="
+
+      </div>
+      
+      <style>
+        
+      </style>
+      
+      `
+
+
+  end
+
+end
