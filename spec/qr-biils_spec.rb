cc6f44010d8ebc09acf3969b9c0f8b86d3febdf5
Damiano Radice <dam.radice@gmail.com>
2020-07-03 22:44:04 +0200

    up test

--- spec/qr-biils_spec.rb
+++ spec/qr-biils_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe QRBills do
     end
   
     it "raise an exception if the bill kind is not valid" do
-      expect{QRBills.new({bill_type: "bad"})}.to raise_error("QR-bill invalid parameters: invalid parameters")
+      expect{QRBills.new({bill_type: "bad"})}.to raise_error("QR-bill invalid parameters: params validation check failed")
     end
   end
 end
