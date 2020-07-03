81b1a27632fe5028cff37add3c417bd6ff504411
Damiano Radice <dam.radice@gmail.com>
2020-07-03 23:57:37 +0200

    fix layout

--- lib/qr-bills/qr-html-layout.rb
+++ lib/qr-bills/qr-html-layout.rb
@@ -6,233 +6,233 @@ class QRHTMLLayout
   end
 
   def self.html_layout(params)
-    layout  = `<div class="bill_container">`
-    layout += `  <div class="receipt_section">`
-    layout += `    <div class="title">#{I18n.t(:receipt)}</div>`
-    layout += `    <div class="subtitle payable_to">#{I18n.t(:account)} / #{I18n.t(:payable_to)}</div>`
-    layout += `    <div class="content">`
-    layout += `      #{params[:bill_params][:creditor][:iban]}<br/>`
-    layout += `      #{params[:bill_params][:creditor][:name]}<br/>`
-    layout += `      #{params[:bill_params][:creditor][:line1]} #{params[:bill_params][:creditor][:line2]}<br/>`
-    layout += `      #{params[:bill_params][:creditor][:postal_code]} #{params[:bill_params][:creditor][:town]}<br/>`
-    layout += `    </div>`
-    layout += `    <div><br/></div>`
+    layout  = "<div class=\"bill_container\">\n"
+    layout += "  <div class=\"receipt_section\">\n"
+    layout += "    <div class=\"title\">#{I18n.t(:receipt).capitalize}</div>\n"
+    layout += "    <div class=\"subtitle payable_to\">#{I18n.t(:account).capitalize} / #{I18n.t(:payable_to).capitalize}</div>\n"
+    layout += "    <div class=\"content\">\n"
+    layout += "      #{params[:bill_params][:creditor][:iban]}<br/>\n"
+    layout += "      #{params[:bill_params][:creditor][:address][:name]}<br/>\n"
+    layout += "      #{params[:bill_params][:creditor][:address][:line1]} #{params[:bill_params][:creditor][:address][:line2]}<br/>\n"
+    layout += "      #{params[:bill_params][:creditor][:address][:postal_code]} #{params[:bill_params][:creditor][:address][:town]}<br/>\n"
+    layout += "    </div>"
+    layout += "    <div><br/></div>"
 
-    if !params[:bill_params][:reference].blank?
-    layout += `    <div class="subtitle reference">#{I18n.t(:reference)}</div>`
-    layout += `    <div class="reference">`
-    layout += `      #{params[:bill_params][:reference]}`
-    layout += `    </div>`
-    layout += `    <div><br/></div>`
+    if !params[:bill_params][:reference].nil? && !params[:bill_params][:reference].empty?
+    layout += "    <div class=\"subtitle reference\">#{I18n.t(:reference).capitalize}</div>\n"
+    layout += "    <div class=\"reference\">\n"
+    layout += "      #{params[:bill_params][:reference]}<br/>\n"
+    layout += "    </div>\n"
+    layout += "    <div><br/></div>\n"
     end
 
-    layout += `    <div class="subtitle payable_by">#{I18n.t(:payable_by)}</div>`
-    layout += `    <div class="payable_by">`
-    layout += `      #{params[:bill_params][:debtor][:name]}<br/>`
-    layout += `      #{params[:bill_params][:debtor][:line1]} #{params[:bill_params][:debtor][:line2]}<br/>`
-    layout += `      #{params[:bill_params][:debtor][:postal_code]} #{params[:bill_params][:debtor][:town]}<br/>`
-    layout += `    </div>`
-    layout += ``
-    layout += `    <div class="amount">`
-    layout += `      <div class="currency">`
-    layout += `        <span class="amount_header subtitle">#{I18n.t(:currency)}</span><br/>`
-    layout += `        #{params[:bill_params][:currency]}`
-    layout += `      </div>`
-    layout += ``
-    layout += `      <div class="amount_value">`
-    layout += `        <span class="amount_header subtitle">#{I18n.t(:amount)}</span><br/>`
-    layout += `        #{params[:bill_params][:amount]}`
-    layout += `      </div>`
-    layout += `    </div>`
-    
-    layout += `    <div class="acceptance_point">`
-    layout += `      #{I18n.t(:acceptance_point)}`
-    layout += `    </div>`
-    
-    layout += `  </div>`
-    layout += `  <div class="payment_section">`
-    layout += `    <div class="left_column">`
-    layout += `      <div class="title">#{I18n.t(:payment_part)}</div>`
-    layout += `      <div class="qr_code"><img src="#{params[:bill_params][:qrcode_filepath]}" /></div>`
-    layout += `      <div class="amount">`
-    layout += `        <div class="currency">`
-    layout += `          <span class="amount_header subtitle">#{I18n.t(:currency)}</span><br/>`
-    layout += `          #{params[:bill_params][:currency]}`
-    layout += `        </div>`
-    
-    layout += `        <div class="amount_value">`
-    layout += `          <span class="amount_header subtitle">#{I18n.t(:amount)}</span><br/>`
-    layout += `          #{params[:bill_params][:amount]}`
-    layout += `        </div>`
-    layout += `      </div>`
-    
-    layout += `      <div class="further_information">`
+    layout += "    <div class=\"subtitle payable_by\">#{I18n.t(:payable_by).capitalize}</div>\n"
+    layout += "    <div class=\"payable_by\">\n"
+    layout += "      #{params[:bill_params][:debtor][:address][:name]}<br/>\n"
+    layout += "      #{params[:bill_params][:debtor][:address][:line1]} #{params[:bill_params][:debtor][:address][:line2]}<br/>\n"
+    layout += "      #{params[:bill_params][:debtor][:address][:postal_code]} #{params[:bill_params][:debtor][:address][:town]}<br/>\n"
+    layout += "    </div>\n"
 
-    if !params[:bill_params][:bill_information_coded].blank?
-    layout += `        <span class="finfo_header">Name AV1:</span> #{params[:bill_params][:bill_information_coded]}`
+    layout += "    <div class=\"amount\">\n"
+    layout += "      <div class=\"currency\">\n"
+    layout += "        <span class=\"amount_header subtitle\">#{I18n.t(:currency).capitalize}</span><br/>\n"
+    layout += "        #{params[:bill_params][:currency]}<br/>\n"
+    layout += "      </div>\n"
+
+    layout += "      <div class=\"amount_value\">\n"
+    layout += "        <span class=\"amount_header subtitle\">#{I18n.t(:amount).capitalize}</span><br/>\n"
+    layout += "        #{params[:bill_params][:amount]}<br/>\n"
+    layout += "      </div>\n"
+    layout += "    </div>\n"
+    
+    layout += "    <div class=\"acceptance_point\">\n"
+    layout += "      #{I18n.t(:acceptance_point).capitalize}<br/>\n"
+    layout += "    </div>\n"
+    
+    layout += "  </div>\n"
+    layout += "  <div class=\"payment_section\">\n"
+    layout += "    <div class=\"left_column\">\n"
+    layout += "      <div class=\"title\">#{I18n.t(:payment_part).capitalize}</div>\n"
+    layout += "      <div class=\"qr_code\"><img src=\"#{params[:bill_params][:qrcode_filepath]}\" /></div>\n"
+    layout += "      <div class=\"amount\">\n"
+    layout += "        <div class=\"currency\">\n"
+    layout += "          <span class=\"amount_header subtitle\">#{I18n.t(:currency).capitalize}</span><br/>\n"
+    layout += "          #{params[:bill_params][:currency]}<br/>\n"
+    layout += "        </div>\n"
+    
+    layout += "        <div class=\"amount_value\">\n"
+    layout += "          <span class=\"amount_header subtitle\">#{I18n.t(:amount).capitalize}</span><br/>\n"
+    layout += "          #{params[:bill_params][:amount]}<br/>\n"
+    layout += "        </div>\n"
+    layout += "      </div>\n"
+    
+    layout += "      <div class=\"further_information\">\n"
+
+    if !params[:bill_params][:bill_information_coded].nil? && !params[:bill_params][:bill_information_coded].empty?
+    layout += "        <span class=\"finfo_header\">Name AV1:</span> #{params[:bill_params][:bill_information_coded]}\n"
     end
   
-    if !params[:bill_params][:alternative_scheme_paramters].blank?
-    layout += `        <span class="finfo_header">Name AV2:</span> #{params[:bill_params][:alternative_scheme_paramters]}`
+    if !params[:bill_params][:alternative_scheme_paramters].nil? && !params[:bill_params][:alternative_scheme_paramters].empty?
+    layout += "        <span class=\"finfo_header\">Name AV2:</span> #{params[:bill_params][:alternative_scheme_paramters]}\n"
     end
 
-    layout += `      </div>`
-    layout += `    </div>`
-    layout += `    <div class="right_column">`
-    layout += `      <div class="subtitle payable_to">#{I18n.t(:account)} / #{I18n.t(:payable_to)}</div>`
-    layout += `      <div class="content">`
-    layout += `        #{params[:bill_params][:creditor][:iban]}<br/>`
-    layout += `        #{params[:bill_params][:creditor][:name]}<br/>`
-    layout += `        #{params[:bill_params][:creditor][:line1]} #{params[:bill_params][:creditor][:line2]}<br/>`
-    layout += `        #{params[:bill_params][:creditor][:postal_code]} #{params[:bill_params][:creditor][:town]}<br/>`
-    layout += `      </div>`
-    layout += `    <div><br/></div>`
+    layout += "      </div>\n"
+    layout += "    </div>\n"
+    layout += "    <div class=\"right_column\">\n"
+    layout += "      <div class=\"subtitle payable_to\">#{I18n.t(:account).capitalize} / #{I18n.t(:payable_to).capitalize}</div>\n"
+    layout += "      <div class=\"content\">\n"
+    layout += "        #{params[:bill_params][:creditor][:iban]}<br/>\n"
+    layout += "        #{params[:bill_params][:creditor][:address][:name]}<br/>\n"
+    layout += "        #{params[:bill_params][:creditor][:address][:line1]} #{params[:bill_params][:creditor][:address][:line2]}<br/>\n"
+    layout += "        #{params[:bill_params][:creditor][:address][:postal_code]} #{params[:bill_params][:creditor][:address][:town]}<br/>\n"
+    layout += "      </div>\n"
+    layout += "    <div><br/></div>\n"
 
-    if !params[:bill_params][:reference].blank?
-    layout += `    <div class="subtitle reference">#{I18n.t(:reference)}</div>`
-    layout += `      <div class="reference">`
-    layout += `        #{params[:bill_params][:reference]}`
-    layout += `      </div>`
-    layout += `    <div><br/></div>`
+    if !params[:bill_params][:reference].nil? && !params[:bill_params][:reference].empty?
+    layout += "    <div class=\"subtitle reference\">#{I18n.t(:reference).capitalize}</div>\n"
+    layout += "      <div class=\"reference\">\n"
+    layout += "        #{params[:bill_params][:reference]}<br/>\n"
+    layout += "      </div>\n"
+    layout += "    <div><br/></div>\n"
     end
 
-    if !params[:bill_params][:additionally_information].blank?
-    layout += `    <div class="subtitle additional_information">#{I18n.t(:additional_information)}</div>`
-    layout += `      <div class="additional_information">`
-    layout += `        #{params[:bill_params][:additionally_information]}`
-    layout += `      </div>`
-    layout += `    <div><br/></div>`
+    if !params[:bill_params][:additionally_information].nil? && !params[:bill_params][:additionally_information].empty?
+    layout += "    <div class=\"subtitle additional_information\">#{I18n.t(:additional_information).capitalize}</div>\n"
+    layout += "      <div class=\"additional_information\">\n"
+    layout += "        #{params[:bill_params][:additionally_information]}<br/>\n"
+    layout += "      </div>\n"
+    layout += "    <div><br/></div>\n"
     end
 
-    layout += `    <div class="subtitle payable_by">#{I18n.t(:payable_by)}</div>`
-    layout += `      <div class="payable_by">`
-    layout += `        #{params[:bill_params][:debtor][:name]}<br/>`
-    layout += `        #{params[:bill_params][:debtor][:line1]} #{params[:bill_params][:debtor][:line2]}<br/>`
-    layout += `        #{params[:bill_params][:debtor][:postal_code]} #{params[:bill_params][:debtor][:town]}<br/>`
-    layout += `      </div>`
-    layout += `    </div>`
-    layout += `  </div>`
-    layout += `</div>`
+    layout += "    <div class=\"subtitle payable_by\">#{I18n.t(:payable_by).capitalize}</div>\n"
+    layout += "      <div class=\"payable_by\">\n"
+    layout += "        #{params[:bill_params][:debtor][:address][:name]}<br/>\n"
+    layout += "        #{params[:bill_params][:debtor][:address][:line1]} #{params[:bill_params][:debtor][:address][:line2]}<br/>\n"
+    layout += "        #{params[:bill_params][:debtor][:address][:postal_code]} #{params[:bill_params][:debtor][:address][:town]}<br/>\n"
+    layout += "      </div>\n"
+    layout += "    </div>\n"
+    layout += "  </div>\n"
+    layout += "</div>\n"
   
-    layout += `<style>`
-    layout += `  @font-face{ `
-    layout += `    font-family: 'liberation_sansregular';`
-    layout += `    src: url('app/assets/fonts/LiberationSans-Regular.eot');`
-    layout += `    src: url('app/assets/fonts/LiberationSans-Regular.eot?#iefix') format('embedded-opentype'),`
-    layout += `        url('app/assets/fonts/LiberationSans-Regular.woff') format('woff'),`
-    layout += `        url('app/assets/fonts/LiberationSans-Regular.ttf') format('truetype'),`
-    layout += `        url('app/assets/fonts/LiberationSans-Regular.svg#liberation_sansregular') format('svg');`
-    layout += `    font-weight: normal;`
-    layout += `    font-style: normal;`
-    layout += `  }`
+    layout += "<style>\n"
+    layout += "  @font-face{ \n"
+    layout += "    font-family: \"liberation_sansregular\";\n"
+    layout += "    src: url(\"app/assets/fonts/LiberationSans-Regular.eot\");\n"
+    layout += "    src: url(\"app/assets/fonts/LiberationSans-Regular.eot?#iefix\") format(\"embedded-opentype\"),\n"
+    layout += "        url(\"app/assets/fonts/LiberationSans-Regular.woff\") format(\"woff\"),\n"
+    layout += "        url(\"app/assets/fonts/LiberationSans-Regular.ttf\") format(\"truetype\"),\n"
+    layout += "        url(\"app/assets/fonts/LiberationSans-Regular.svg#liberation_sansregular\") format(\"svg\");\n"
+    layout += "    font-weight: normal;\n"
+    layout += "    font-style: normal;\n"
+    layout += "  }\n"
 
-    layout += `  .bill_container {`
-    layout += `    width: 210mm;`
-    layout += `    height: 105mm;`
-    layout += `    font-family: 'liberation_sansregular';`
-    layout += `   border: 1px solid #ccc;`
-    layout += `  }`
+    layout += "  .bill_container {\n"
+    layout += "    width: 210mm;\n"
+    layout += "    height: 105mm;\n"
+    layout += "    font-family: \"liberation_sansregular\";\n"
+    layout += "   border: 1px solid #ccc;\n"
+    layout += "  }\n"
     
-    layout += `  .bill_container:after {`
-    layout += `    content: "";`
-    layout += `    display: table;`
-    layout += `    clear: both;`
-    layout += `  }`
+    layout += "  .bill_container:after {\n"
+    layout += "    content: \"\";\n"
+    layout += "    display: table;\n"
+    layout += "    clear: both;\n"
+    layout += "  }\n"
     
-    layout += `  .receipt_section {`
-    layout += `    width: 52mm;`
-    layout += `    height: 95mm;`
-    layout += `    padding: 5mm;`
+    layout += "  .receipt_section {\n"
+    layout += "    width: 52mm;\n"
+    layout += "    height: 95mm;\n"
+    layout += "    padding: 5mm;\n"
     
-    layout += `    float: left;`
+    layout += "    float: left;\n"
     
-    layout += `    font-size: 8pt;`
+    layout += "    font-size: 8pt;\n"
     
-    layout += `    border-right: 1px dotted #ccc;`
-    layout += `  }`
+    layout += "    border-right: 1px dotted #ccc;\n"
+    layout += "  }\n"
     
-    layout += ` .payment_section {`
-    layout += `    width: 137mm;`
-    layout += `    height: 95mm;`
-    layout += `    float: left;`
-    layout += `    padding: 5mm;`
+    layout += " .payment_section {\n"
+    layout += "    width: 137mm;\n"
+    layout += "    height: 95mm;\n"
+    layout += "    float: left;\n"
+    layout += "    padding: 5mm;\n"
     
-    layout += `    font-size: 10pt;`
-    layout += `  }`
+    layout += "    font-size: 10pt;\n"
+    layout += "  }\n"
     
-    layout += `  .payment_section .left_column {`
-    layout += `    height: 95mm;`
-    layout += `    width: 46mm;`
-    layout += `    float: left;`
+    layout += "  .payment_section .left_column {\n"
+    layout += "    height: 95mm;\n"
+    layout += "    width: 46mm;\n"
+    layout += "    float: left;\n"
     
-    layout += `    margin-right: 5mm;`
-    layout += `  }`
+    layout += "    margin-right: 5mm;\n"
+    layout += "  }\n"
     
-    layout += `  .payment_section .right_column {`
-    layout += `    height: 95mm;`
-    layout += `    width: 86mm;`
-    layout += `    float: left;`
-    layout += `  }`
+    layout += "  .payment_section .right_column {\n"
+    layout += "    height: 95mm;\n"
+    layout += "    width: 86mm;\n"
+    layout += "    float: left;\n"
+    layout += "  }\n"
     
-    layout += `  .qr_code {`
-    layout += `    padding: 5mm 0mm 5mm 0mm;`
+    layout += "  .qr_code {\n"
+    layout += "    padding: 5mm 0mm 5mm 0mm;\n"
     
-    layout += `    height: 46mm;`
-    layout += `    width: 46mm;`
-    layout += `  }`
+    layout += "    height: 46mm;\n"
+    layout += "    width: 46mm;\n"
+    layout += "  }\n"
     
-    layout += `  .qr_code img {`
-    layout += `    height: 46mm;`
-    layout += `    width: 46mm;`
-    layout += `  }`
+    layout += "  .qr_code img {\n"
+    layout += "    height: 46mm;\n"
+    layout += "    width: 46mm;\n"
+    layout += "  }\n"
     
-    layout += `  .amount {`
-    layout += `    margin-top: 15px;`
-    layout += `  }`
+    layout += "  .amount {\n"
+    layout += "    margin-top: 15px;\n"
+    layout += "  }\n"
     
-    layout += `  .amount .currency {`
-    layout += `    float: left;`
-    layout += `    margin-right: 15px;`
-    layout += `  }`
+    layout += "  .amount .currency {\n"
+    layout += "    float: left;\n"
+    layout += "    margin-right: 15px;\n"
+    layout += "  }\n"
     
-    layout += `  .title {`
-    layout += `    font-weight: bold;`
-    layout += `    font-size: 11pt;`
-    layout += `  }`
+    layout += "  .title {\n"
+    layout += "    font-weight: bold;\n"
+    layout += "    font-size: 11pt;\n"
+    layout += "  }\n"
     
-    layout += `  .receipt_section .subtitle {`
-    layout += `    font-weight: bold;`
-    layout += `    font-size: 6pt;`
-    layout += `    line-height: 9pt;`
-    layout += `  }`
+    layout += "  .receipt_section .subtitle {\n"
+    layout += "    font-weight: bold;\n"
+    layout += "    font-size: 6pt;\n"
+    layout += "    line-height: 9pt;\n"
+    layout += "  }\n"
     
-    layout += `  .receipt_section .acceptance_point {`
-    layout += `    font-weight: bold;`
-    layout += `    text-align: right;`
-    layout += `    font-size: 6pt;`
-    layout += `    line-height: 8pt;`
-    layout += `    padding-top: 5mm;`
-    layout += `  }`
+    layout += "  .receipt_section .acceptance_point {\n"
+    layout += "    font-weight: bold;\n"
+    layout += "    text-align: right;\n"
+    layout += "    font-size: 6pt;\n"
+    layout += "    line-height: 8pt;\n"
+    layout += "    padding-top: 5mm;\n"
+    layout += "  }\n"
     
-    layout += `  .payment_section .subtitle {`
-    layout += `    font-weight: bold;`
-    layout += `    font-size: 8pt;`
-    layout += `    line-height: 11pt;`
-    layout += `  }`
+    layout += "  .payment_section .subtitle {\n"
+    layout += "    font-weight: bold;\n"
+    layout += "    font-size: 8pt;\n"
+    layout += "    line-height: 11pt;\n"
+    layout += "  }\n"
     
-    layout += `  .payment_section .amount {`
-    layout += `    height: 22mm;`
-    layout += `  }`
+    layout += "  .payment_section .amount {\n"
+    layout += "    height: 22mm;\n"
+    layout += "  }\n"
 
-    layout += `  .payment_section .further_information {`
-    layout += `    font-size: 7pt;`
-    layout += `  }`
+    layout += "  .payment_section .further_information {\n"
+    layout += "    font-size: 7pt;\n"
+    layout += "  }\n"
     
-    layout += `  .payment_section .finfo_header {`
-    layout += `    font-weight: bold;`
-    layout += `  }      `
-    layout += `</style>`
+    layout += "  .payment_section .finfo_header {\n"
+    layout += "    font-weight: bold;\n"
+    layout += "  }      \n"
+    layout += "</style>\n"
 
-    layout
+    return layout
   end
 end
