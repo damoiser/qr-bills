29a928808f85d677e686cf23c0c244a874e21dd7
Damiano Radice <dam.radice@gmail.com>
2020-07-05 17:52:59 +0200

    rename create func

--- spec/qr-biils_spec.rb
+++ spec/qr-biils_spec.rb
@@ -3,11 +3,11 @@ require 'qr-bills'
 RSpec.describe QRBills do
   describe "init" do
     it "raise an exception if the bill kind is not set" do
-      expect{QRBills.new({})}.to raise_error("QR-bill invalid parameters: bill type param not set")
+      expect{QRBills.generate({})}.to raise_error("QR-bill invalid parameters: bill type param not set")
     end
   
     it "raise an exception if the bill kind is not valid" do
-      expect{QRBills.new({bill_type: "bad"})}.to raise_error("QR-bill invalid parameters: params validation check failed")
+      expect{QRBills.generate({bill_type: "bad"})}.to raise_error("QR-bill invalid parameters: params validation check failed")
     end
   end
 end
