f200527cd09f7282cfe1310e0aff2e072924dcfd
Damiano Radice <dam.radice@gmail.com>
2020-07-11 16:21:35 +0200

    add tests for creditor reference

---
+++ spec/qr-creditor-reference_spec.rb
@@ -0,0 +1,28 @@
+require 'qr-bills'
+require 'qr-bills/qr-creditor-reference'
+
+RSpec.describe "QRCreditorReference" do
+  describe "validate the input correctly" do
+    it "checks that reference should be less than 21 chars" do
+      expect{QRCreditorReference.create("ABCDABCDABCDABCDABCDABCD")}.to raise_error
+    end
+
+    it "checks that reference should be greater or equal than 1 char" do
+      expect{QRCreditorReference.create("")}.to raise_error
+    end
+  end
+
+  describe "produce the correct reference" do
+    it "produces the right one [1]" do
+      expect(QRCreditorReference.create("MTR81UUWZYO48NY55NP3")).to eq("RF89MTR81UUWZYO48NY55NP3")
+    end
+
+    it "produces the right one [2]" do
+      expect(QRCreditorReference.create("539007547034")).to eq("RF18539007547034")
+    end
+
+    it "produces the right one [3]" do
+      expect(QRCreditorReference.create("539 007 5470 34 ")).to eq("RF18539007547034")
+    end
+  end
+end
